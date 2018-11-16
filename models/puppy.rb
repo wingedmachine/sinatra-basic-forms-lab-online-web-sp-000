d74dc324137d8e75afe3b870ea95047fe5360fff
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 01:09:31 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,6 @@
 class Puppy
-  attr_accessor :age
-  attr_reader :name, :breed
+  attr_accessor :name, :age
+  attr_reader :breed
   def initialize(options)
     @name = options[:name]
     @breed = options[:breed]
